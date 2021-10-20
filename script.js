388c995853c9d9cd275cb6bda55150726c502cf5
Hemant Makkar <gautam.makkar2001@gmail.com>
2021-10-20 16:34:01 +0530

    update

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ emailBtn.addEventListener("click", e=> {
     // sendMail();
 })
 
-// setInterval(getExchangeRate, 5*60*1000);
-// setInterval(sendMail, 60*60*1000);
+setInterval(getExchangeRate, 5*60*1000);
+setInterval(sendMail, 60*60*1000);
 
 function getExchangeRate(){
     const amount = document.querySelector("form input");
